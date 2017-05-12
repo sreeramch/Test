018aa662987319b4f4ca33d7da3724be3a672a47
Sreeram Chinta <sreeram.chinta@gmail.com>
2017-05-11 19:02:29 -0500

    Tokenize an std::string in an efficient way
    The parsing and tokenizing mechanism is fast and light-weight. Utility function tokenize() returns a vector of token strings. 
    Word of caution: No trim mechanism is implemented for simplicity purposes. You might end up with spaces in one or more token strings.

---
+++ tokenize.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<string> tokenize(const string &sstring)
+{
+	size_t pos, startAt;
+	pos = startAt = 0;
+
+	vector<string> vstrings;
+	string item;
+	while ((pos = sstring.find('|', startAt)) != string::npos)
+    {
+    	item = sstring.substr(startAt, pos-startAt);
+    	#ifdef DEBUGAPP
+    		cout << "item: " << item << endl;
+    	#endif
+    	vstrings.push_back(item);
+    	startAt = pos+1;
+    }
+
+    #ifdef DEBUGAPP
+    	cout << "item: " << sstring.substr(startAt) << endl;
+    #endif
+
+    vstrings.push_back(sstring.substr(startAt));
+
+    return vstrings;
+}
