14845e3b549e82c75edd166e767f52ed08783a58
Sreeram Chinta <sreeram.chinta@gmail.com>
2017-05-11 19:05:52 -0500

    Fixed formatting.

--- tokenize.cpp
+++ tokenize.cpp
@@ -6,23 +6,23 @@ using namespace std;
 
 vector<string> tokenize(const string &sstring, char delimiter)
 {
-	size_t pos, startAt;
-	pos = startAt = 0;
+    size_t pos, startAt;
+    pos = startAt = 0;
 
-	vector<string> vstrings;
-	string item;
-	while ((pos = sstring.find(delimiter, startAt)) != string::npos)
+    vector<string> vstrings;
+    string item;
+    while ((pos = sstring.find(delimiter, startAt)) != string::npos)
     {
     	item = sstring.substr(startAt, pos-startAt);
     	#ifdef DEBUGAPP
-    		cout << "item: " << item << endl;
+    	  cout << "item: " << item << endl;
     	#endif
     	vstrings.push_back(item);
     	startAt = pos+1;
     }
 
     #ifdef DEBUGAPP
-    	cout << "item: " << sstring.substr(startAt) << endl;
+      cout << "item: " << sstring.substr(startAt) << endl;
     #endif
 
     vstrings.push_back(sstring.substr(startAt));
